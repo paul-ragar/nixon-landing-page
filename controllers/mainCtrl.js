6175b7bbcd2f97f5917836c9f0d015e7c1c15c2a
Paul Ragar <pragar@Pauls-MacBook-Pro.local>
2016-12-18 23:10:21 -0700

    initial commit

---
+++ controllers/mainCtrl.js
@@ -0,0 +1,25 @@
+angular.module('myApp').controller('mainCtrl', function(mainService, $scope, $location, $anchorScroll) {
+
+
+  $scope.scrollToTop = () => {
+    console.log("Go to top!");
+    // set the location.hash to the id of
+    // the element you wish to scroll to.
+    $location.hash('top');
+    // call $anchorScroll()
+    $anchorScroll();
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+// END OF CONTROLLER
+});
